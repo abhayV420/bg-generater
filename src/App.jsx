fba6f7b31a26e3950cbe2628fef32b222cf571e6
Abhay <abhayV420@outlook.com>
2024-09-26 12:17:23 +0530

    Made my 1st React Project with 'Vite' and with following dependencies:- 'Tailwind', 'Bootstrap'

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
+let newC = (color) =>{
+  setColor(color)
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+     <div className='fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2'>
+       <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+           <button type="button" className="btn btn-primary" onClick={()=>{newC('blue')}}>Blue</button>
+           <button type="button" className="btn btn-success" onClick={()=>{newC('green')}}>Green</button>
+           <button type="button" className="btn btn-warning" onClick={()=>{newC('yellow')}}>Yellow</button>
+
+       </div>
+     </div>
+  </div>
   )
 }
 
